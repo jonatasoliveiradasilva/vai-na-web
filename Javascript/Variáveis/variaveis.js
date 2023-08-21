c1e4a93dff3d0d527ea86dfb9b8c98dbf248cff1
Jônatas Oliveira da Silva <oliveirajonatas@outlook.com.br>
2023-08-21 12:12:22 -0300

    variáveis

---
+++ Javascript/Variáveis/variaveis.js
@@ -0,0 +1,56 @@
+// variáveis são onde armazenamos os dados
+// palavra chave + nome da variável + dado que eu quero guardar
+
+var cor = "azul";
+cor = "pink";
+
+console.log(cor);
+
+// var = escopo global
+// let = escopo de bloco (ex: função)
+// const = não pode ser atribuído um novo valor
+
+let nome = "Maria";
+
+nome = "Grazy";
+nome = "Murilo";
+
+console.log(nome);
+
+
+
+const maiorDeIdade = false;
+
+console.log(maiorDeIdade);
+console.log(typeof maiorDeIdade);
+
+
+
+let turmaT1 = "MARAVILHOSA!";
+
+console.log(turmaT1);
+
+
+
+const nomeAluno = "Denis";
+
+console.log(nomeAluno);
+
+
+
+let comidaFavorita = "Lasanha";
+
+console.log(comidaFavorita);
+
+console.log(`Meu aluno ${nomeAluno} gosta de ${comidaFavorita}`);
+
+
+
+// tipos de dados
+
+// string: texto (sequência de caracteres (aspas simples e/ou aspas duplas)
+// number: número (números inteiros, positivos, negativos, decimais (reais))
+// boolean: true (verdadeiro (1)) false (falso (0))
+// null: um dado nulo (inexistente)
+// undefined: indefinido (não foi definido) variável criada e não atribuída com algum valor
+// NaN: Not A Number (não é um número)
