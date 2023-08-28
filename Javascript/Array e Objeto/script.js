49a8ff0bef5548b693cd0981ec73685329ca9581
Jônatas Oliveira da Silva <oliveirajonatas@outlook.com.br>
2023-08-28 11:08:43 -0300

    atualização - array e objeto

--- Javascript/Array e Objeto/script.js
+++ Javascript/Array e Objeto/script.js
@@ -7,7 +7,7 @@ console.log(numeros[2], numeros[4]);
 
 // sort
 
-console.log (
+console.log(
     numeros.sort(function (x, y) {
         return x - y;
     }),
@@ -38,3 +38,72 @@ console.log(objeto.nome); // indice nomeado
 console.log(objeto.profissão); // indice nomeado
 console.log(objeto.idade); // indice nomeado
 console.log(objeto.moradia); // indice nomeado
+
+
+
+// array []
+// é uma coleção de dados
+// em um array podemos armazenar vários dados
+// acesssar o array pelo index (indice/posição)
+
+let lista = ["Caneta Vermelha", "Marcadores Coloridos", "Cadernos"];
+console.log(lista);
+
+console.log(lista.includes("Cadernos"));
+// vai procurar se "cadernos" está incluido no array
+// retorna (true/false)
+
+console.log(lista.indexOf("Cadernos"));
+// retorna a posição de "cadernos"
+
+// quando colocamos o nome do array e o . ele vai mostrar vários métodos que podemos usar
+
+
+
+// objeto {}
+// coleção de propriedades
+// propriedades são divididas em chave e valor
+
+let listaCompras = {
+    caneta: "Vermelha",
+    marcadores: "Coloridos",
+}
+
+console.log(listaCompras);
+
+let funcionario = {
+    nome: "Roberto",
+    salário: 3000,
+    função: "Gerente",
+    matricula: 45,
+};
+
+console.log(funcionario);
+
+// para remover uma propriedade no objeto
+delete funcionario.matricula;
+console.log(funcionario);
+
+let cadastro = [
+    {
+        nome: "Kleber",
+        idade: 22,
+        profissão: "Facilitador Front End"
+    },
+    {
+        nome: "Grazy",
+        idade: 40,
+        profissão: "Facilitadora Front End"
+    },
+    {
+        nome: "Carol",
+        idade: 29,
+        profissão: "Instrutora Front End"
+    }
+];
+
+console.log(cadastro);
+console.log(cadastro[2]);
+console.log(cadastro[2].profissão);
+
+console.log(`${cadastro[0].nome} gosta de jogar video game!`);
