e8bba9458c76a9d487ec653aca11bbcaa2d3d516
Jônatas Oliveira da Silva <oliveirajonatas@outlook.com.br>
2023-10-04 13:37:08 -0300

    aula 3

---
+++ React/aula 3/t1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../src/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <h1>Testando...</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ React/aula 3/t1/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// arrow function (função de seta)
+const Header = () => {
+    return (
+        <>
+            <p>Eu sou a header</p>
+        </>
+    );
+}
+
+export default Header;
